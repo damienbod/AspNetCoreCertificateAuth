e719ea90045ec38771500dda5d6481e921d4bfec
damienbod <damien_bod@hotmail.com>
2019-07-05 08:02:05 +0200

    adding a httpClient test with handler

---
+++ AspNetCoreCertificateAuthHandler/ApiService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCertificateAuthHandler
+{
+    public class ApiService
+    {
+        private readonly IOptions<AuthConfigurations> _authConfigurations;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly ApiTokenInMemoryClient _apiTokenInMemoryClient;
+
+        public ApiService(
+            IOptions<AuthConfigurations> authConfigurations, 
+            IHttpClientFactory clientFactory,
+            ApiTokenInMemoryClient apiTokenClient)
+        {
+            _authConfigurations = authConfigurations;
+            _clientFactory = clientFactory;
+            _apiTokenInMemoryClient = apiTokenClient;
+        }
+
+        public async Task<JArray> GetApiDataAsync()
+        {
+            try
+            {
+                var clientCertificate = new X509Certificate2("child_b_from_a_dev_damienbod.pfx", "1234");
+                //var clientCertificate = new X509Certificate2("child_a_dev_damienbod.pfx", "1234");
+                
+                //var access_token = await _apiTokenInMemoryClient.GetApiToken(
+                //    "ProtectedApi",
+                //    "protected_scope",
+                //    "protected_secret"
+                //);
+
+                var client = _clientFactory.CreateClient("api");
+                var response = await client.GetAsync("https://localhost:44378/api/values");
+
+                //var request = new HttpRequestMessage()
+                //{
+                //    RequestUri = new Uri("https://localhost:44378/api/values"),
+                //    Method = HttpMethod.Get,
+                //};
+
+                //request.Headers.Add("X-ARR-ClientCert", clientCertificate.GetRawCertDataString());
+                //var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var data = JArray.Parse(responseContent);
+
+                    return data;
+                }
+
+                throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
+            }
+            catch (Exception e)
+            {
+                throw new ApplicationException($"Exception {e}");
+            }
+        }
+    }
+}
---
+++ AspNetCoreCertificateAuthHandler/Startup.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCoreCertificateAuthHandler
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+            });
+
+            services.AddTransient<ApiService>();
+            services.AddSingleton<ApiTokenInMemoryClient>();
+
+            services.Configure<AuthConfigurations>(Configuration.GetSection("AuthConfigurations"));
+            var clientCertificate = new X509Certificate2("child_a_dev_damienbod.pfx", "1234");
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(clientCertificate);
+            handler.SslProtocols = SslProtocols.Tls11 | SslProtocols.Tls12;
+            handler.CheckCertificateRevocationList = false;
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+
+            using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser, OpenFlags.ReadWrite))
+            {
+                X509Certificate2Collection coll = new X509Certificate2Collection();
+                coll.Import("child_a_dev_damienbod.pfx", "1234", X509KeyStorageFlags.DefaultKeySet);
+
+                foreach (X509Certificate2 cert in coll)
+                {
+                    if (!cert.HasPrivateKey)
+                    {
+                        store.Add(cert);
+                    }
+
+                    cert.Dispose();
+                }
+            }
+
+            services.AddHttpClient("api", c =>
+            {
+                ///c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }).ConfigurePrimaryHttpMessageHandler(() => handler);
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
